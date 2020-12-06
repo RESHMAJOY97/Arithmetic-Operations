2f89b7bf36e72fc31a78d967a2077d98bf9f9fe4
RESHMAJOY97 <reshmajoy97@gmail.com>
2020-12-06 15:43:49 +0530

    success

---
+++ index.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="css/style.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Arithmetic Operations</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+</head>
+<body class="bgcolor">
+    <nav class="navbar navbar-expand-sm bg-light navbar-light">
+        <ul class="navbar-nav">
+          <li class="nav-item active">
+            <a class="nav-link" href="http://127.0.0.1:5500/index.html">HOME</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="http://127.0.0.1:5500/addition.html">ADDITION</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="http://127.0.0.1:5500/subtraction.html">SUBTRACTION</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="http://127.0.0.1:5500/division.html">DIVISION</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="http://127.0.0.1:5500/multiplication.html">MULTIPLICATION</a>
+          </li>
+        </ul>
+      </nav> 
+      ARITHMETIC OPERATIONS  
+
+    Arithmetic  is a branch of mathematics that consists of the study of numbers, especially the properties of the traditional operations on them—addition, subtraction, multiplication, division, exponentiation and extraction of roots.Arithmetic is an elementary part of number theory, and number theory is considered to be one of the top-level divisions of modern mathematics, along with algebra, geometry, and analysis. The terms arithmetic and higher arithmetic were used until the beginning of the 20th century as synonyms for number theory, and are sometimes still used to refer to a wider part of number theory.
+    The basic arithmetic operations are addition, subtraction, multiplication, and division. Arithmetic is performed according to an order of operations.[1]  
+   <div class="container">
+          <div class="row">
+              <div class="col col-sm-3">
+    
+    Addition, denoted by the symbol {\displaystyle +}+, is the most basic operation of arithmetic. In its simple form, addition combines two numbers, the addends or terms, into a single number, the sum of the numbers (such as 2 + 2 = 4 or 3 + 5 = 8).
+    
+    Adding finitely many numbers can be viewed as repeated simple addition; this procedure is known as summation, a term also used to denote the definition for "adding infinitely many numbers" in an infinite series. Repeated addition of the number 1 is the most basic form of counting; the result of adding 1 is usually called the successor of the original number.
+    
+    Addition is commutative and associative, so the order in which finitely many terms are added does not matter. The identity element for a binary operation is the number that, when combined with any number, yields the same number as the result. According to the rules of addition, adding 0 to any number yields that same number, so 0 is the additive identity.[1] The inverse of a number with respect to a binary operation is the number that, when combined with any number, yields the identity with respect to this operation. So the inverse of a number with respect to addition (its additive inverse, or the opposite number) is the number that yields the additive identity, 0, when added to the original number; it is immediately obvious that for all numbers {\displaystyle x}x, this is the negative of {\displaystyle x}x (denoted {\displaystyle -x}-x).[1] For example, the additive inverse of 7 is −7, since 7 + (−7) = 0.
+    
+    Addition can also be interpreted geometrically, as in the following example:
+    
+    If we have two sticks of lengths 2 and 5, then, if we place the sticks one after the other, the length of the stick becomes 7, since 2 + 5 = 7.
+   </div>
+   <div class="col col-sm-3">
+  
+    Subtraction, denoted by the symbol {\displaystyle -}-, is the inverse operation to addition. Subtraction finds the difference between two numbers, the minuend minus the subtrahend: D = M − S. Resorting to the previously established addition, this is to say that the difference is the number that, when added to the subtrahend, results in the minuend: D + S = M.[2]
+    
+    For positive arguments M and S holds:
+    
+    If the minuend is larger than the subtrahend, the difference D is positive.
+    If the minuend is smaller than the subtrahend, the difference D is negative.
+    In any case, if minuend and subtrahend are equal, the difference D = 0.
+    
+    Subtraction is neither commutative nor associative. For that reason, the construction of this inverse operation in modern algebra is often discarded in favor of introducing the concept of inverse elements (as sketched under § Addition), where subtraction is regarded as adding the additive inverse of the subtrahend to the minuend, that is, a − b = a + (−b). The immediate price of discarding the binary operation of subtraction is the introduction of the (trivial) unary operation, delivering the additive inverse for any given number, and losing the immediate access to the notion of difference, which is potentially misleading when negative arguments are involved.
+    
+    For any representation of numbers, there are methods for calculating results, some of which are particularly advantageous in exploiting procedures, existing for one operation, by small alterations also for others. For example, digital computers can reuse existing adding-circuitry and save additional circuits for implementing a subtraction, by employing the method of two's complement for representing the additive inverses, which is extremely easy to implement in hardware (negation). The trade-off is the halving of the number range for a fixed word length.
+    
+    A formerly wide spread method to achieve a correct change amount, knowing the due and given amounts, is the counting up method, which does not explicitly generate the value of the difference. Suppose an amount P is given in order to pay the required amount Q, with P greater than Q. Rather than explicitly performing the subtraction P − Q = C and counting out that amount C in change, money is counted out starting with the successor of Q, and continuing in the steps of the currency, until P is reached. Although the amount counted out must equal the result of the subtraction P − Q, the subtraction was never really done and the value of P − Q is not supplied by this method.
+   </div>
+   <div class="col col-sm-3">
+   
+    Multiplication, denoted by the symbols {\displaystyle \times }\times  or {\displaystyle \cdot }\cdot ,[1] is the second basic operation of arithmetic. Multiplication also combines two numbers into a single number, the product. The two original numbers are called the multiplier and the multiplicand, mostly both are simply called factors.
+    
+    Multiplication may be viewed as a scaling operation. If the numbers are imagined as lying in a line, multiplication by a number greater than 1, say x, is the same as stretching everything away from 0 uniformly, in such a way that the number 1 itself is stretched to where x was. Similarly, multiplying by a number less than 1 can be imagined as squeezing towards 0, in such a way that 1 goes to the multiplicand.
+    
+    Another view on multiplication of integer numbers (extendable to rationals but not very accessible for real numbers) is by considering it as repeated addition. For example. 3 × 4 corresponds to either adding 3 times a 4, or 4 times a 3, giving the same result. There are different opinions on the advantageousness of these paradigmata in math education.
+    
+    Multiplication is commutative and associative; further, it is distributive over addition and subtraction. The multiplicative identity is 1,[1] since multiplying any number by 1 yields that same number. The multiplicative inverse for any number except 0 is the reciprocal of this number, because multiplying the reciprocal of any number by the number itself yields the multiplicative identity 1. 0 is the only number without a multiplicative inverse, and the result of multiplying any number and 0 is again 0. One says that 0 is not contained in the multiplicative group of the numbers.
+    
+    The product of a and b is written as a × b or a·b. When a or b are expressions not written simply with digits, it is also written by simple juxtaposition: ab.[1] In computer programming languages and software packages (in which one can only use characters normally found on a keyboard), it is often written with an asterisk: a * b.
+    
+    Algorithms implementing the operation of multiplication for various representations of numbers are by far more costly and laborious than those for addition. Those accessible for manual computation either rely on breaking down the factors to single place values and applying repeated addition, or on employing tables or slide rules, thereby mapping multiplication to addition and vice versa. These methods are outdated and are gradually replaced by mobile devices. Computers utilize diverse sophisticated and highly optimized algorithms, to implement multiplication and division for the various number formats supported in their system.
+   </div>
+   <div class="col col-sm-3">
+  
+    Division, denoted by the symbols {\displaystyle \div }\div  or {\displaystyle /}/,[1] is essentially the inverse operation to multiplication. Division finds the quotient of two numbers, the dividend divided by the divisor. Any dividend divided by zero is undefined. For distinct positive numbers, if the dividend is larger than the divisor, the quotient is greater than 1, otherwise it is less than 1 (a similar rule applies for negative numbers). The quotient multiplied by the divisor always yields the dividend.
+    
+    Division is neither commutative nor associative. So as explained in § Subtraction, the construction of the division in modern algebra is discarded in favor of constructing the inverse elements with respect to multiplication, as introduced in § Multiplication. Hence division is the multiplication of the dividend with the reciprocal of the divisor as factors, that is, a ÷ b = a × 
+    1/b.
+   </div>
+          </div>
+   </div>
+</body>
+</html>
---
+++ subtraction.php
@@ -0,0 +1,82 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="css/style.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Subtraction</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+</head>
+<body class="bgcolor">
+    <nav class="navbar navbar-expand-sm bg-light navbar-light">
+        <ul class="navbar-nav">
+          <li class="nav-item active">
+            <a class="nav-link" href="http://127.0.0.1:5500/index.html">HOME</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="http://127.0.0.1:5500/addition.html">ADDITION</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="http://127.0.0.1:5500/subtraction.html">SUBTRACTION</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="http://127.0.0.1:5500/division.html">DIVISION</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="http://127.0.0.1:5500/multiplication.html">MULTIPLICATION</a>
+          </li>
+        </ul>
+      </nav>
+    <div class="container">
+        <div class="row">
+            <div class="col ">
+
+            </div>
+            <div class="col">
+                <table class="table table-borderless">
+                    <h1>SUBTRACTION</h1>
+                    <tr>
+                        <td>FIRST NUMBER</td>
+                        <td><input type="text" class="form-control" id="n1"></td>
+                    </tr>
+                    <tr>
+                        <td>SECOND NUMBER</td>
+                        <td><input type="text" class="form-control" id="n2"></td>
+                    </tr>
+                    <tr>
+                        <td>DIFFERENCE</td>
+                        <td><input type="text" class="form-control" id="difference"></td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td><button onClick=buttonclick() type="button" class="btn btn-danger">DIFFERENCE</button>
+                        </td>
+                    </tr>
+                </table>
+               
+            </div>
+            <div class="col ">
+
+            </div>
+        </div>
+    </div>
+    <script>
+        function buttonclick() {
+           var getn1=parseInt(document.getElementById("n1").value)
+           var getn2=parseInt(document.getElementById("n2").value)
+           console.log(getn1)
+           console.log(getn2)
+           if(Number.isInteger(getn1 && getn2)){
+              result=getn1-getn2
+              document.getElementById("difference").value=result
+            }
+            else
+            {
+              alert("Enter a number")
+            }
+        }
+        
+    </script>
+</body>
+</html>
